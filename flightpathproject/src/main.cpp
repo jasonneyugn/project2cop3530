dae4504e21632f1cc5c3c13c4e79ac4a548e49cb
jasonneyugn <theknifingn@gmail.com>
2025-11-08 17:03:55 -0500

    Added the ability to click nodes and they change colors.

--- flightpathproject/src/main.cpp
+++ flightpathproject/src/main.cpp
@@ -5,6 +5,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <cmath>
+#include <unordered_map>
 
 struct Airport {
     int id;
@@ -12,6 +13,13 @@ struct Airport {
     double lat;
     double lon;
 };
+
+
+struct Edge {
+    int dest;
+    double weight;
+};
+
 
 
 static std::string get_field(std::stringstream& s) { // helper to parse CSV
@@ -27,28 +35,92 @@ static std::string get_field(std::stringstream& s) { // helper to parse CSV
     return out;
 }
 
+std::unordered_map<int, Airport*> makeMap(std::vector<Airport>& airports) {
+    std::unordered_map<int, Airport*> map;
+    for (auto& airport : airports) {
+        map[airport.id] = &airport;
+    }
+    return map;
+}
+
 
 sf::Vector2f toPixel(double lat, double lon, float width, float height) {
     double latRad = lat * M_PI / 180.0 ;  //mercator projection formula from stack overflow
     double mercN = std::log(std::tan((M_PI / 4.0) + (latRad / 2.0)));
     double y = (height / 2.0) - (width * mercN / (2.0 * M_PI));
-    double x = (lon + 180.0) * (width / 360.0);
+    double x = (lon + 180.5) * (width / 360.0);
     return sf::Vector2f(static_cast<float>(x), static_cast<float>(y));
 }
 
 
+static double haversine(double lat1, double lon1,    // from geeks for geeks
+                        double lat2, double lon2)
+{
+    double dLat = (lat2 - lat1) *
+                  M_PI / 180.0;
+    double dLon = (lon2 - lon1) *
+                  M_PI / 180.0;
+
+    lat1 = (lat1) * M_PI / 180.0;
+    lat2 = (lat2) * M_PI / 180.0;
+
+    double a = pow(sin(dLat / 2), 2) +
+               pow(sin(dLon / 2), 2) *
+               cos(lat1) * cos(lat2);
+    double rad = 6371;
+    double c = 2 * asin(sqrt(a));
+    return rad * c;
+}
+
+
+void loadRoutes(const std::string& filename, std::unordered_map<int, std::vector<Edge>>& adj,
+                const std::unordered_map<int, Airport*>& airportById) {
+    std::ifstream routes(filename);
+    if (!routes.is_open()) {
+        std::cerr << "Error: could not open airports.dat\n";
+        return;
+    }
+
+    std::string line;
+    while (std::getline(routes, line)) {
+        std::stringstream ss(line);
+        get_field(ss);
+        get_field(ss);
+        get_field(ss);
+        std::string src_id = get_field(ss);
+        get_field(ss);
+        std::string dest_id = get_field(ss);
+
+        if (src_id.empty() || dest_id.empty()) {
+            continue;
+        }
+        int srcid = 0, destid = 0;
+        try {
+            srcid = std::stoi(src_id);
+            destid = std::stoi(dest_id);
+        } catch (...) {
+            continue;
+        }
+        if (!airportById.count(srcid) || !airportById.count(destid))
+            continue;
+
+        const Airport* A = airportById.at(srcid);
+        const Airport* B = airportById.at(destid);
+
+        double dist = haversine(A->lat, A->lon, B->lat, B->lon);
+        adj[srcid].push_back({destid, dist});
+      }
+}
 
 
 int main() {
     std::vector<Airport> airports;
-
+    std::string line;
     std::ifstream file("./data/airports.dat");
     if (!file.is_open()) {
         std::cerr << "Error: could not open airports.dat\n";
         return 1;
     }
-
-    std::string line;
     while (std::getline(file, line)) {
         std::stringstream ss(line);
         std::string cell;
@@ -78,6 +150,12 @@ int main() {
         airports.push_back(a);
     }
 
+
+    auto airportById = makeMap(airports);
+    std::unordered_map<int, std::vector<Edge>> adj;
+    loadRoutes("./data/routes.dat", adj, airportById);
+
+
     sf::RenderWindow window(sf::VideoMode(1200, 1200), "OpenFlights Viewer"); // window for map
     window.setFramerateLimit(60);
 
@@ -94,11 +172,23 @@ int main() {
     dots.reserve(airports.size());
     for (auto& airport : airports) {
         auto p = toPixel(airport.lat, airport.lon, mapW, mapH);
-        sf::CircleShape dot(1.5f);
-        dot.setPosition(p.x -1.5f, p.y-1.5f);
+        sf::CircleShape dot(2.f);
+        dot.setPosition(p.x -2.f, p.y-2.f);
         dot.setFillColor(sf::Color::Red);
         dots.push_back(dot);
     }
+
+    int selectedAirport = -1;
+    std::unordered_map<int, sf::Vector2f> airportPos;
+
+    for (auto& airport : airports) {
+        auto p = toPixel(airport.lat, airport.lon, mapW, mapH);
+        sf::CircleShape dot(1.5f);
+        dot.setPosition(p.x - 1.5f, p.y - 1.5f);
+        dot.setFillColor(sf::Color::Red);
+        airportPos[airport.id] = p; //
+    }
+
 
     sf::View view = window.getDefaultView();
     float zoom = 1.0f;
@@ -138,7 +228,7 @@ int main() {
                 window.setView(view);
             }
             if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {   // mouse panning from stack overflow
-                 dragging = true;
+                dragging = true;
                 mousePos = sf::Mouse::getPosition(window);
             }
             else if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
@@ -151,12 +241,51 @@ int main() {
                 window.setView(view);
                 mousePos = currMouse;
             }
+            if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2f clickPos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                float clickRadius = 2.0f;
+                selectedAirport = -1;
 
-            window.clear(sf::Color::Black);
-            window.draw(map);
-            for (auto dot: dots)
-                window.draw(dot);
-            window.display();
+                for (auto& airport : airports) {
+                    sf::Vector2f pos = airportPos[airport.id];
+                    if (std::hypot(pos.x - clickPos.x, pos.y - clickPos.y) < clickRadius) {
+                        selectedAirport = airport.id;
+                    }
+                }
+            }
         }
+        window.clear(sf::Color::Black);
+        window.draw(map);
+        for (size_t i = 0; i < airports.size(); i++) {
+            auto& airport = airports[i];
+            auto dot = dots[i];
+
+            if (airport.id == selectedAirport) {
+                dot.setFillColor(sf::Color::Cyan);
+            }
+            else
+                dot.setFillColor(sf::Color::Red);
+
+            dot.setScale(zoom, zoom);
+            window.draw(dot);
+        }
+        if (selectedAirport != -1 && adj.count(selectedAirport)) {
+            sf::VertexArray lines(sf::Lines);
+            for (auto& e : adj[selectedAirport]) {
+                if (!airportPos.count(e.dest)) {
+                    continue;
+                }
+                lines.append(sf::Vertex(airportPos[selectedAirport], sf::Color::Yellow));
+                lines.append(sf::Vertex(airportPos[e.dest], sf::Color::Yellow));
+            }
+            window.draw(lines);
+            sf::CircleShape dotnew(2.f);
+            dotnew.setFillColor(sf::Color::Blue);
+            dotnew.setPosition(airportPos[selectedAirport].x - 2.f, airportPos[selectedAirport].y - 2.f);
+            window.draw(dotnew);
+        }
+        window.display();
     }
-}
+
+    }
+
